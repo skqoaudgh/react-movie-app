77b3d0071ab5bc37e24eeef86cfdcedd8fe12109
myungho <skqoaudgh2@hanmail.net>
2020-04-06 17:33:18 +0900

    클래스이름 수정 및 CSS 파일 분리

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
 export const Movie = ({ id, year, title, summary, poster, genres }) => {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <p className="movie_summary">{summary}</p>
-        <ul className="genres">
-          {genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => <li key={index}>{genre}</li>)}
         </ul>
       </div>
     </div>
